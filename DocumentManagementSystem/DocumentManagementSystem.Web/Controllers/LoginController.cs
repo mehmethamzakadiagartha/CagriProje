dc7e4e727a8600699d82e582dffbb6efab0085d7
mehmethamzakadiagartha <mehmet.hamzakadi@agarthateknoloji.com.tr>
2020-11-14 19:00:18 +0300

    Web katmanı için login ekranı ve altyapı oluşturuldu

---
+++ DocumentManagementSystem/DocumentManagementSystem.Web/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using DocumentManagementSystem.Web.Models.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentManagementSystem.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            //Login kontrolleri burada yapılacak.
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DocumentManagementSystem/DocumentManagementSystem.Web/Models/Login/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocumentManagementSystem.Web.Models.Login
+{
+    public class LoginViewModel
+    {
+        public string UserMail { get; set; }
+
+        public string UserPassword { get; set; }
+    }
+}
